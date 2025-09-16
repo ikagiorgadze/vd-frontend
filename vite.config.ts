1569978a2c09736dc46c40b7376a70aae2aebfc1
Irakli Giorgadze <ik48giorgadze@gmail.com>
2025-09-16 11:10:31 +0400

    a

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,14 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      // Proxy /api/* → http://localhost:3000/*
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), 
+      },
+    },
   },
   plugins: [react()],
   resolve: {
